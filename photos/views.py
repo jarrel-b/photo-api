8f0d3b6b79656cdb712407d1214025efb0652a4d
jarrel-b <jarrel.biscocho@gmail.com>
2020-02-11 18:35:07 -0800

    Remove unneeded kwarg

--- photos/views.py
+++ photos/views.py
@@ -9,6 +9,4 @@ def list_catalog(request):
     page_size = request.GET.get("page_size")
     last_token = request.GET.get("last_token")
     catalog = data.query_catalog(page_size, last_token)
-    return JsonResponse(
-        status=200, content_type="application/json", data=catalog,
-    )
+    return JsonResponse(status=200, data=catalog)
